c3cb216c3f5c9128ccff5c6a729721047348503e
minseong2 <48282846+minseong2@users.noreply.github.com>
2024-03-06 19:07:41 +0900

    Add files via upload

---
+++ chat_server/SocCom.cpp
@@ -0,0 +1,29 @@
+﻿// SocCom.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ChatServer.h"
+#include "SocCom.h"
+
+
+// CSocCom
+
+CSocCom::CSocCom()
+{
+}
+
+CSocCom::~CSocCom()
+{
+}
+
+
+// CSocCom 멤버 함수
+
+void CSocCom::Init(HWND hWnd) {
+	m_hWnd = hWnd;
+}
+void CSocCom::OnReceive(int nErrorCode)
+{
+	SendMessage(m_hWnd, UM_RECEIVE, m_index, 0);
+	CSocket::OnReceive(nErrorCode);
+}
